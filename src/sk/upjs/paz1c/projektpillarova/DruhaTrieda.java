edf893168e6bc0059ad404954aebbd23f4e03e47
Filip <Filip@158.197.232.31>
2022-09-28 18:36:31 +0200

    Test failu

--- src/sk/upjs/paz1c/projektpillarova/DruhaTrieda.java
+++ src/sk/upjs/paz1c/projektpillarova/DruhaTrieda.java
@@ -5,7 +5,7 @@ public class DruhaTrieda {
 	public static void main(String[] args) {
 		System.out.println("Ahoj");
 		System.out.println("Na slovensku po slovensky");
-
+		System.out.println("V madarsku po madarsky");
 	}
 
 }
