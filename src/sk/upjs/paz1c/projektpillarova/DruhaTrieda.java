f12bd04f7f93ce8b1d5e175de53a0ee1f5b909d9
Filip <Filip@158.197.232.31>
2022-09-28 18:28:32 +0200

    Edit druhej triedy

--- src/sk/upjs/paz1c/projektpillarova/DruhaTrieda.java
+++ src/sk/upjs/paz1c/projektpillarova/DruhaTrieda.java
@@ -3,7 +3,8 @@ package sk.upjs.paz1c.projektpillarova;
 public class DruhaTrieda {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Ahoj");
+		System.out.println("Na slovensku po slovensky");
 
 	}
 
